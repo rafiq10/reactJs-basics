36a80b49fb07a771aa10ac4e1634b3502c031bc8
rafiq10 <rafiq10@gmail.com>
2018-03-11 16:52:11 +0100

    difficult one

--- reactjs-basics/src/components/home.js
+++ reactjs-basics/src/components/home.js
@@ -7,7 +7,8 @@ export class Home extends React.Component {
         super();
         this.state = {
             age: props.initielaAge,
-            status: 0
+            status: 0,
+            homeLink: "Changed Link"
         };
         setTimeout(() => {
             this.setState({
@@ -21,6 +22,11 @@ export class Home extends React.Component {
         }); 
         console.log(this.age);
     }
+
+    onChangeName(){
+        this.props.changeLink(this.state.homeLink);
+    }
+
     render() {
         return(
             <div>
@@ -32,6 +38,8 @@ export class Home extends React.Component {
                 </button>
                 <hr/>
                 <button onClick={this.props.greet} className="btn btn-primary" >Greet</button>
+                <button onClick={this.onChangeName.bind(this)} className="btn btn-primary" >change header link</button>
+                
             </div>
         );
     }
--- reactjs-basics/src/index.js
+++ reactjs-basics/src/index.js
@@ -6,17 +6,28 @@ import  { Home } from "./components/home";
 import {Container, Row, Col} from 'reactstrap';
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            homeLink: "Home"
+        };
+    }
+
     onGreet(){
         alert("nice!");
     }
-
+    onChangeLinkName(newName){
+        this.setState({
+            homeLink: newName
+        });
+    }
     render () 
     {
         return (
             <Container>
                 <Row>
                     <Col>
-                        <Header homeLink="Home"/>
+                        <Header homeLink={this.state.homeLink}/>
                     </Col>
                 </Row>
                 <br/>
@@ -24,7 +35,12 @@ class App extends React.Component {
                 <br/>
                 <Row>
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Rafal"} initielaAge={34} greet={this.onGreet}/>
+                        <Home 
+                            name={"Rafal"} 
+                            initielaAge={34} 
+                            greet={this.onGreet}
+                            changeLink={this.onChangeLinkName.bind(this)}
+                        />
                     </div>
                 </Row>
             </Container>
